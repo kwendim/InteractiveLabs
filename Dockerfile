e4958cbb0214f8b4bd2328ea9b2640dd753185d2
Kidus Mammo <kidus.mammo@aalto.fi>
2020-03-11 14:52:46 +0200

    Integrated webhooks

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
+RUN mkdir /root/.ssh/
+COPY ./knownhosts/* /root/.ssh/
+RUN chmod 400 /root/.ssh/id_rsa
--- labs/urls.py
+++ labs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from . import webhooks
+from django.views.decorators.csrf import csrf_exempt #TODO check security implications of exempting csrf tokens
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -12,4 +15,5 @@ urlpatterns = [
     path('accounts/signup/', views.MySignupView.as_view(), name='account_signup'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/', include('allauth.urls')),
+    path('webhooks', csrf_exempt(webhooks.index), name="webhook") #TODO this path should only be accessible from github somehow
 ]
